f29c79c0409c463a6a23f67b43eb52fff7b22bbd
Victor <fatalmc14@gmail.com>
2025-07-17 13:33:00 +0300

    LessonsForAutotesting№3

--- test_site.py
+++ test_site.py
@@ -1,5 +1,26 @@
 # https://demoblaze.com/
 # https://magento.softwaretestingboard.com/
+# https://www.qa-practice.com/
+import time
 
-def test_opes_s6():
-    return None
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import links
+
+
+
+@pytest.fixture()
+def driver():
+    browser = webdriver.Firefox()
+    browser.maximize_window()
+    yield browser
+    time.sleep(10)
+    browser.close()
+
+def test_open_nigger(driver):
+    driver.get(links.magento)
+    Argus = driver.find_element(By.LINK_TEXT, 'Argus All-Weather Tank')
+    Argus.click()
+    title = driver.find_element(By.CLASS_NAME, 'base')
+    assert title.text == 'Argus All-Weather Tank'
